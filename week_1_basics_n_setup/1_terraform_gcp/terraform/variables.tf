296d7ab3d1bce39593cc2bf7f38026a2c329873f
waleed <waleed.ayoub@gmail.com>
2023-03-03 16:43:16 -0500

    fixed conflicts in terraform

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -3,34 +3,23 @@ locals {
 }
 
 variable "project" {
-<<<<<<< HEAD
   description = "possible-lotus-375803"
-=======
-  description = "Your GCP Project ID"
->>>>>>> class/main
 }
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-<<<<<<< HEAD
-  default = "northamerica-northeast1"
-=======
-  default = "europe-west6"
->>>>>>> class/main
-  type = string
+  default     = "northamerica-northeast1"
+  type        = string
 }
 
 variable "storage_class" {
   description = "Storage class type for your bucket. Check official docs for more info."
-  default = "STANDARD"
+  default     = "STANDARD"
 }
 
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
-  type = string
-  default = "trips_data_all"
-<<<<<<< HEAD
+  type        = string
+  default     = "trips_data_all"
 }
-=======
-}
->>>>>>> class/main
+
